65aab3ac62189cf0bceada6dd6b4691ce996e988
Стамо Петков <sgpetkov@is-bg.net>
2022-03-04 17:04:00 +0200

    Add project files.

---
+++ Warehouse/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Warehouse.Core.Constants;
+using Warehouse.Models;
+
+namespace Warehouse.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.SuccessMessage] = "Браво, успяхме да подкараме тостера!";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
