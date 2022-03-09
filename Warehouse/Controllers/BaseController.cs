fa8c5a7a4115aea0bff667838ad5753d7c58d3ae
Стамо Петков <sgpetkov@is-bg.net>
2022-03-09 20:25:15 +0200

    Views customized

---
+++ Warehouse/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Warehouse.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
--- Warehouse/Controllers/HomeController.cs
+++ Warehouse/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using Warehouse.Models;
 
 namespace Warehouse.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
